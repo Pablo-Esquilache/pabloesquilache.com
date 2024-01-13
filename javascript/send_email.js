e4ebec68673951a208f48bbbd1e47d79a8a3598b
Pablo <pabloesquilache@gmail.com>
2024-01-13 00:38:04 -0300

    responsiveee

--- javascript/send_email.js
+++ javascript/send_email.js
@@ -1,29 +1,30 @@
-
-
 export function sendEmail() {
+  document.addEventListener("DOMContentLoaded", function () {
     const btn = document.getElementById("button");
-  
-    document.getElementById("form").addEventListener("submit", function (e) {
-      e.preventDefault();
-  
-      btn.value = "Enviando...";
-  
-      const serviceID = "default_service";
-      const templateID = "template_kym0z0o";
-  
-      emailjs.sendForm(serviceID, templateID, this).then(
-        () => {
-          btn.value = "Mensaje enviado!";
-          setTimeout(() => {
+
+    if (btn) {
+      document.getElementById("form").addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        btn.value = "Enviando...";
+
+        const serviceID = "default_service";
+        const templateID = "template_kym0z0o";
+
+        emailjs.sendForm(serviceID, templateID, this).then(
+          () => {
+            btn.value = "Mensaje enviado!";
+            setTimeout(() => {
+              btn.value = "Enviar";
+              form.reset();
+            }, 1000);
+          },
+          (err) => {
             btn.value = "Enviar";
-            form.reset();
-          }, 1000);
-        },
-        (err) => {
-          btn.value = "Enviar";
-          alert(JSON.stringify(err));
-        }
-      );
-    });
-  }
-  
+            alert(JSON.stringify(err));
+          }
+        );
+      });
+    }
+  });
+}
