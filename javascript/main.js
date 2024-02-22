357794a67721d838734952ae2566fb89e8b4eff9
Pablo <pabloesquilache@gmail.com>
2024-02-22 13:10:56 -0300

    para que vea sandri

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 import { manejarNavegacionResponsive } from "./menu_resposive.js";
 // import { slider } from './slider.js';
-import { activeMenu } from "./active_menu.js";
+// import { activeMenu } from "./active_menu.js";
 import { scrollScreen } from "./scroll_screen.js";
 import { formContact } from "./form_contact.js";
 import { sendEmail } from "./send_email.js";
@@ -8,7 +8,7 @@ import { sendEmail } from "./send_email.js";
 window.addEventListener("DOMContentLoaded", () => {
   manejarNavegacionResponsive();
   //slider();
-  activeMenu();
+  // activeMenu();
   scrollScreen();
   formContact();
   sendEmail();
