0c64d05fe09b84540b9ac0d689f1883b7a397a39
Pablo <pabloesquilache@gmail.com>
2024-02-29 13:59:58 -0300

    sobre mi ahora si

--- javascript/modal.js
+++ javascript/modal.js
@@ -4,6 +4,8 @@ export function modal() {
   const isQuienSoyPage = window.location.pathname.includes("sobre_mi.html");
 
   if (isQuienSoyPage) {
+    
+      const body = document.body;
       const modal_container = document.getElementById("modal-container");
       const quiensoy_card1 = document.getElementById("quiensoy-card1");
       const quiensoy_card2 = document.getElementById("quiensoy-card2");
@@ -14,32 +16,41 @@ export function modal() {
   
       quiensoy_h2_1.addEventListener("click", () => {
         modal_container.style.display = "flex";
+        modal_container.style.pointerEvents = "auto";
         quiensoy_card1.style.display = "flex";
+        body.classList.add('modal-open');
       });
   
       quiensoy_card1.addEventListener("click", () => {
         quiensoy_card1.style.display = "none";
         modal_container.style.display = "none";
+        body.classList.remove('modal-open');
       });
   
       quiensoy_h2_2.addEventListener("click", () => {
         modal_container.style.display = "flex";
+        modal_container.style.pointerEvents = "auto";
         quiensoy_card2.style.display = "flex";
+        body.classList.add('modal-open');
       });
   
       quiensoy_card2.addEventListener("click", () => {
         quiensoy_card2.style.display = "none";
         modal_container.style.display = "none";
+        body.classList.remove('modal-open');
       });
   
       quiensoy_h2_3.addEventListener("click", () => {
         modal_container.style.display = "flex";
+        modal_container.style.pointerEvents = "auto";
         quiensoy_card3.style.display = "flex";
+        body.classList.add('modal-open');
       });
   
       quiensoy_card3.addEventListener("click", () => {
         quiensoy_card3.style.display = "none";
         modal_container.style.display = "none";
+        body.classList.remove('modal-open');
       });
     }
   }
