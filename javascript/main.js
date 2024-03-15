11f0f44c74305023c3d3871773a240789050cb3a
Pablo <pabloesquilache@gmail.com>
2024-03-14 23:34:15 -0300

    img servicios

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,5 @@
 import { manejarNavegacionResponsive } from "./menu_resposive.js";
-import { slider } from "./slider.js";
+import { initializeSlider } from "./slider.js";
 // import { activeMenu } from "./active_menu.js";
 import { scrollScreen } from "./scroll_screen.js";
 import { formContact } from "./form_contact.js";
@@ -8,7 +8,7 @@ import { sendEmail } from "./send_email.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   manejarNavegacionResponsive();
-  slider();
+  initializeSlider();
   // activeMenu();
   scrollScreen();
   // modal();
--- javascript/slider.js
+++ javascript/slider.js
@@ -1,30 +1,62 @@
-export function slider() {
-  const slider = document.querySelector(".slider");
-  const totalSlides = document.querySelectorAll(".slider img").length;
-  let currentIndex = 0;
+// export function slider() {
+//   const slider = document.querySelector(".slider");
+//   const totalSlides = document.querySelectorAll(".slider img").length;
+//   let currentIndex = 0;
+
+//   function showSlide(index) {
+//     currentIndex = (index + totalSlides) % totalSlides;
+//     const translateValue = -currentIndex * 100 + "%";
+//     slider.style.transform = "translateX(" + translateValue + ")";
+//   }
+
+//   function nextSlide() {
+//     showSlide(currentIndex + 1);
+//   }
+
+//   function prevSlide() {
+//     showSlide(currentIndex - 1);
+//   }
+
+//   const btnNext = document.getElementById("btn-next");
+//   const btnPrev = document.getElementById("btn-prev");
+
+//   // Verificar si los botones existen antes de agregar los event listeners
+//   if (btnNext && btnPrev) {
+//     btnNext.addEventListener("click", nextSlide);
+//     btnPrev.addEventListener("click", prevSlide);
+//   }
+
+//   setInterval(nextSlide, 10000);
+// }
+
+export function initializeSlider() {
+  const slides = document.querySelectorAll('.slider img');
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  let currentSlide = 0;
 
   function showSlide(index) {
-    currentIndex = (index + totalSlides) % totalSlides;
-    const translateValue = -currentIndex * 100 + "%";
-    slider.style.transform = "translateX(" + translateValue + ")";
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.display = 'block';
+      } else {
+        slide.style.display = 'none';
+      }
+    });
   }
 
   function nextSlide() {
-    showSlide(currentIndex + 1);
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
   }
 
   function prevSlide() {
-    showSlide(currentIndex - 1);
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
   }
 
-  const btnNext = document.getElementById("btn-next");
-  const btnPrev = document.getElementById("btn-prev");
+  nextBtn.addEventListener('click', nextSlide);
+  prevBtn.addEventListener('click', prevSlide);
 
-  // Verificar si los botones existen antes de agregar los event listeners
-  if (btnNext && btnPrev) {
-    btnNext.addEventListener("click", nextSlide);
-    btnPrev.addEventListener("click", prevSlide);
-  }
-
-  setInterval(nextSlide, 10000);
+  setInterval(nextSlide, 5000);
 }
